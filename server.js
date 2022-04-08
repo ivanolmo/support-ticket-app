efbb9288a4158896fd3f53fb3e6fa9a52c529cc2
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-08 18:55:55 -0500

    Add starter server functionality

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('server running');
+const dotenv = require('dotenv').config();
+const express = require('express');
+const { connectDB } = require('./config/db');
+const { errorHandler } = require('./utils/errorHandler');
+
+const PORT = process.env.PORT || 5500;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (request, response) => {
+  response.status(200).send('Support Ticket API...');
+});
+
+// ROUTES //
+app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
