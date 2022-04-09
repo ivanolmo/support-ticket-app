5bb6586e2d6e903e2c6a107c0140ff0968eb2f99
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-09 18:20:00 -0500

    Add Login and Register pages
    Each page has form to allow new user registration or existing user login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please login to create your support request</p>
+      </section>
+      <section className='form' onSubmit={onSubmit}>
+        <form>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              name='email'
+              placeholder='Email'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              name='password'
+              placeholder='Password'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+import { FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const { name, email, password, passwordConfirm } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== passwordConfirm) {
+      toast.error('Password mismatch');
+    }
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className='form' onSubmit={onSubmit}>
+        <form>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              value={name}
+              name='name'
+              placeholder='Name'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              value={email}
+              name='email'
+              placeholder='Email'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              value={password}
+              name='password'
+              placeholder='Password'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='passwordConfirm'
+              value={passwordConfirm}
+              name='passwordConfirm'
+              placeholder='Confirm password'
+              required
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
+
+export default Register;
