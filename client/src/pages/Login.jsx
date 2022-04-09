c8bbe034d51ebedac7416be31b1baeb16a603ce4
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-09 18:51:46 -0500

    Refactor pages with login and register reducers
    Pages are using useSelector and useDispatch redux hooks

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { FaSignInAlt } from 'react-icons/fa';
 import { toast } from 'react-toastify';
+
+import { login } from '../features/auth/authSlice';
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -9,6 +12,12 @@ function Login() {
   });
 
   const { email, password } = formData;
+
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -19,6 +28,10 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = { email, password };
+
+    dispatch(login(userData));
   };
 
   return (
