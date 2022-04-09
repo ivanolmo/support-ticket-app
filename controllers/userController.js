b73724a6d02fe7db5bbb5950cdaad9be9e17c0a2
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-08 19:22:17 -0500

    Refactor controller to allow user to register
    Does simple validation for presence of name, email, and password
    WIll reject if any of those are missing
    Will also reject if user email already exists

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,42 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (request, response) => {
-  const { user, password, email } = request.body;
+  const { name, password, email } = request.body;
 
-  if (!user || !password || !email) {
+  if (!name || !password || !email) {
     // testing
     response.status(400);
     throw new Error('Error in registerUser');
   }
 
-  response.send('User registration controller');
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    response.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    response.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    response.status(400);
+    throw new Error('Error creating user');
+  }
 });
 
 const loginUser = asyncHandler(async (request, response) => {
