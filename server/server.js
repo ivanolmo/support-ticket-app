41eb8e6c389549d8463eadacfabf0015ef9d8cc9
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-09 17:12:21 -0500

    Move package.json to project root

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config({ path: '../.env' });
+const dotenv = require('dotenv').config();
 const express = require('express');
 const { connectDB } = require('./config/db');
 const { errorMiddleware } = require('./middleware/errorMiddleware');
