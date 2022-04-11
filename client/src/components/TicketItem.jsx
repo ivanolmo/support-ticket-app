ef8ff8cd353d2129899e572f178b2ec453bc7ce2
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-10 20:38:11 -0500

    Add TicketItem component to render each ticket

---
+++ client/src/components/TicketItem.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function TicketItem({ ticket }) {
+  const { _id, product, status, createdAt } = ticket;
+
+  return (
+    <div className='ticket'>
+      <div>{new Date(createdAt).toLocaleString('en-US')}</div>
+      <div>{product}</div>
+      <div className={`status status-${status}`}>{status}</div>
+      <Link to={`/ticket/${_id}`} className='btn btn-reverse btn-sm'>
+        View
+      </Link>
+    </div>
+  );
+}
+
+export default TicketItem;
