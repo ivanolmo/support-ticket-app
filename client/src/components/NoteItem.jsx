08305be1b1863c54288631f04ad5e0fd8767d310
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-11 21:40:08 -0500

    Add new NoteItem component that renders single note

---
+++ client/src/components/NoteItem.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+
+import React from 'react';
+
+function NoteItem({ note }) {
+  const { user } = useSelector((state) => state.auth);
+
+  const isStaff = note.isStaff;
+
+  return (
+    <div className={`note ${isStaff ? 'note-staff' : ''}`}>
+      <h4>
+        Note from: {user.name}
+        {isStaff ? <span class='staff-name'> (Staff Member )</span> : ''}
+      </h4>
+      <p>{note.text}</p>
+      <div className='note-date'>
+        {new Date(note.createdAt).toLocaleString('en-US')}
+      </div>
+    </div>
+  );
+}
+
+export default NoteItem;
