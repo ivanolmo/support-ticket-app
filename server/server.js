696b1b3e924836b9fb4a93f2aaadcb5521e3397d
Ivan Olmo <ivan.olmo@gmail.com>
2022-04-13 23:47:25 -0500

    Refactor server for heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const { connectDB } = require('./config/db');
 const { errorMiddleware } = require('./middleware/errorMiddleware');
 
@@ -11,14 +12,27 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (request, response) => {
-  response.status(200).send('Support Ticket API...');
+app.get('/', (_, response) => {
+  response.status(200).send('Support Ticket API');
 });
 
 // ROUTES //
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 app.use(errorMiddleware);
+
+// serve front end folder when in production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (_, response) =>
+    response.sendFile(path.join(__dirname, '../client/build/index.html'))
+  );
+} else {
+  app.get('/', (_, response) => {
+    response.status(200).json({ message: 'Support Ticket API' });
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
